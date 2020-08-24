f5c684937d5a6e0b0533b8212a8c8d491ca778e6
angelsblack45 <angelsblack45@gmail.com>
2020-08-24 14:55:39 +0700

    AMP

---
+++ nuxt.config.js
@@ -0,0 +1,84 @@
+export default {
+  /*
+   ** Nuxt rendering mode
+   ** See https://nuxtjs.org/api/configuration-mode
+   */
+  mode: 'universal',
+  /*
+   ** Nuxt target
+   ** See https://nuxtjs.org/api/configuration-target
+   */
+  target: 'server',
+  /*
+   ** Headers of the page
+   ** See https://nuxtjs.org/api/configuration-head
+   */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || '',
+      },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   ** https://nuxtjs.org/guide/plugins
+   */
+  plugins: [],
+  /*
+   ** Auto import components
+   ** See https://nuxtjs.org/api/configuration-components
+   */
+  components: true,
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: ['@nuxtjs/pwa', '@nuxtjs/amp'],
+  pwa: {
+    meta: {
+      title: 'BMIC COMPANY',
+      author: 'LongNN',
+    },
+    manifest: {
+      name: 'BMIC Modules',
+      short_name: 'BMIC Modules',
+      lang: 'en',
+      display: 'fullscreen',
+      start_url: '/',
+      theme_color: '#e76f51',
+      background_color: '#e76f51',
+      icons: [
+        {
+          src: './static/icon.png',
+          sizes: '196x196',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+      ],
+    },
+  },
+  amp: {
+    origin: 'http://localhost:3000',
+    mode: 'hybrid',
+  },
+
+  /*
+   ** Build configuration
+   ** See https://nuxtjs.org/api/configuration-build/
+   */
+  build: {},
+}
---
+++ pages/amp.vue
@@ -0,0 +1,21 @@
+<template>
+    <div v-if="this.$isAMP">
+        <h1>AMP Page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    amp: 'hybrid',
+    ampLayout: 'amp',
+    mounted() {
+        if(this.$isAMP){
+            console.log(this.$isAMP);
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
